9ea7b23a082d1860c3c85e966471a3d613075616
Tanvi <tanvi29virani@gmail.com>
2025-07-13 12:44:42 +0530

    conversions of datatypes

---
+++ 03_conversion_operations.js
@@ -0,0 +1,47 @@
+//let score = "Tanvi"
+// let score = "100"; // string
+// let score = 100; // number
+// let score = true; // boolean
+// let score = null; // null
+// let score = undefined; // undefined
+// let score = "33abc"; // string with non-numeric characters
+
+
+// console.log (typeof score);
+// console.log (typeof(score ));// as a method peranthesis ni andar 
+
+// let valueInNumber = Number(score);
+// console.log(valueInNumber); // NaN (Not a Number)
+// console.log(typeof valueInNumber); // number
+
+// "33" -> 33 easily convert into number
+// "33abc" -> NaN because it has non-numeric characters
+// true -> 1
+// false -> 0
+// null -> 0
+// undefined -> NaN
+// "100" -> 100 
+// "Tanvi" is a string, so it will be converted to NaN when using Number()
+
+
+//-----------------------------------------BOOLEAN---------------------------------------------------------
+
+// let isLoggedIn = 1
+//  let   isLoggedInBoolean = Boolean(isLoggedIn);
+//  console.log(isLoggedInBoolean ) ==> // true
+
+// let isLoggedIn = ""
+// let isLoggedInBoolean = Boolean(isLoggedIn);
+// console.log(isLoggedInBoolean); // false
+
+// let isLoggedIn = "Tanvi"
+// let isLoggedInBoolean = Boolean(isLoggedIn);
+// console.log(isLoggedInBoolean); // true
+
+//------------------------------------string---------------------------------------------------------
+
+let someNumber = 33
+
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
