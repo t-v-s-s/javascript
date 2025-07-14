294acb9d5b0ca0cf98aa1e876e91f25936eea07d
Tanvi <tanvi29virani@gmail.com>
2025-07-14 13:13:16 +0530

    comparisions

--- 03_conversion_operations.js
+++ 03_conversion_operations.js
@@ -40,8 +40,46 @@
 
 //------------------------------------string---------------------------------------------------------
 
-let someNumber = 33
+/*let someNumber = 33
 
 let stringNumber = String(someNumber)
 console.log(stringNumber);
-console.log(typeof stringNumber);
+console.log(typeof stringNumber);*/
+
+//---------------------------------Operations---------------------------------------------------------
+//  let value = 3
+//  let negValue = -value
+ // console.log (negValue)
+
+//  console.log(2+2);
+//  console.log(2-2);
+//  console.log(2*2);
+//  console.log(2**3);
+//  console.log(4/4);
+//  console.log(4%2);
+
+//----------------------------------String Concatenation---------------------------------------------------------
+
+//  let str1 = "Helllo"
+//  let str2 = " Tanvi"
+//  let str3 = str1 + str2
+//  console.log(str3)
+
+//  console.log("1" + 2);
+//  console .log(1 + "2");
+//  console.log("1" + 2 + 2);
+//  console.log(1 + 2 + "2");
+//  console.log((3+4)* 5 % 3);
+
+//  console.log(True);
+//  console.log(+true);
+//  console.log(+"");
+
+ let num1, num2, num3;
+ num1 = num2 = num3 = 2+2
+
+ let gameCounter = 100
+ gameCounter++; //(post fix)
+ ++gameCounter; //(pre fix)
+ console.log(gameCounter);
+ 
