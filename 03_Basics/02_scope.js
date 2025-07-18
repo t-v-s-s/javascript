2fcec5ea68f84249d1ba83256f8e1597bd55e6c5
Tanvi <tanvi29virani@gmail.com>
2025-07-18 06:17:10 +0530

    scope

--- 03_Basics/02_scope.js
+++ 03_Basics/02_scope.js
@@ -1,8 +1,39 @@
-let a = 300
-if(true){
-     let a = 30
-     const b = 20
-     console.log("INNER : " ,a);
+// let a = 300
+// if(true){
+//      let a = 30
+//      const b = 20
+//      console.log("INNER : " ,a);
 
+
+
+// }
+// console.log("OUTER : " ,a);
+
+// ------------------ Nested Scope ----------------------------
+
+function one (){
+     const username = "Tanu"
+
+     function two (){
+        const website = 'youtube'
+        console.log(username);
+     }
+     //console.log(website); // This will throw an error because 'website' is not defined in this scope
+     //two()
 }
-console.log("OUTER : " ,a);
+//one()
+
+
+
+if (true){
+    username = "takuu";
+
+    if (username === "takuu"){
+        website = "twitter";
+        console.log(username + website)
+    }
+
+    console.log("Inside block:", website); // ✅
+}
+
+console.log("Outside block:", username); // ✅
